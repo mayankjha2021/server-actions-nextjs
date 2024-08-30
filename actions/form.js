ee6bb1154783415306847a768d5dbb8a38b8332b
mayankjha2021 <mayankk.ug21.ee@nitp.ac.in>
2024-08-30 18:46:48 +0530

    first commit

---
+++ actions/form.js
@@ -0,0 +1,10 @@
+"use server"
+import fs from "fs/promises"
+
+  export const submitAction = async (e)=>{
+    console.log(e.get("name"),e.get("add"))
+    let a =await fs.writeFile("harry.txt", `Name is ${e.get
+        ("name")} and Address is ${e.get("add")}`)
+        console.log(a)
+        
+  }
